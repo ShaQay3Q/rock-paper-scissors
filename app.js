230ce7f1cc9e346b98e710d56c45b5c2d2634b47
ShaQay3Q <shaghay3gh@gmail.com>
2024-12-09 13:30:24 +0100

    getting userInput and returning onr of the existing uptions

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
 const startGameBtn = document.getElementById("start-game-btn");
 
+const choices = {
+	1: "rock",
+	2: "paper",
+	3: "scissors",
+};
+
+const userInput = prompt(
+	`Choose between:
+        1 for "rock"
+        2 for "paper"
+        3 for "scissors"`,
+	""
+);
+
+console.log("userInput: " + userInput);
+console.log(!!userInput);
+
+const theInput = parseInt(userInput);
+
+console.log(!!theInput);
+
+if (!!userInput && !!theInput && 1 <= theInput && theInput <= 3) {
+	console.log("inside if: " + theInput);
+	console.log(choices[theInput]);
+}
+
 function startGame() {
 	console.log("Game is starting...");
 }
 
-const person = {
-	// Method: function attach to an object
-	greet: function greet() {
-		console.log("Hello!");
-	},
-};
+// const person = {
+// 	// Method: function attach to an object
+// 	greet: function greet() {
+// 		console.log("Hello!");
+// 	},
+// };
 
-person.greet();
-console.dir(startGame);
+// person.greet();
+// console.dir(startGame);
 
 startGameBtn.addEventListener("click", startGame);
